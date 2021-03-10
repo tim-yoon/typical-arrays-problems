59070c6e5b2ad3230d0df09ee37dc035b9a807f8
Timur Ismayelov <fiske6r@mail.ru>
2021-03-10 18:00:18 +0500

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  if (!array || !array.length) {
   return 0;
+}
+  let mini = Math.min(...array);
+  return mini;
 }
 
 exports.max = function max (array) {
+  if (!array || !array.length) {
   return 0;
+}
+  let maxi = Math.max(...array);
+  return maxi;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
